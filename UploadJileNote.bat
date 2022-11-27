c145616f5095164e548255386a38eb35c193fa64
jilefo <jilefo@outlook.com>
2022-11-27 16:16:50 +0800

    Auto commit.

---
+++ UploadJileNote.bat
@@ -0,0 +1,14 @@
+git config --global user.name "jilefo"
+git config --global user.email "jilefo@outlook.com"
+git remote add gitee git@gitee.com:jilefo/Alistman.git
+rem git pull --rebase origin master
+rem git pull --rebase github master
+git add .
+rem git branch --set-upstream-to origin/master
+rem git rebase --continue
+git commit -m "Auto commit."
+rem git push -u origin master
+rem git push -u github master
+git pull origin master
+git push origin master
+git push -f origin master
